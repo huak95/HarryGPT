b2155431fa593ef2ad005bb2ec8f845e692672c0
huak95 <huak95@gmail.com>
2024-03-16 14:36:30 +0700

    add init comment

---
+++ bigram.py
@@ -0,0 +1,21 @@
+# pytorch=1.12.1 (py3.9_cuda11.3_cudnn8.3.2_0)
+
+# hyperparameters
+
+# load data from wget
+
+# encode decode
+
+# train and test split
+
+# data loading
+
+# estimate loss by eval (train and test)
+
+# simple bigram model
+
+#  create optimizer
+
+# training loops
+
+# generate from the model
